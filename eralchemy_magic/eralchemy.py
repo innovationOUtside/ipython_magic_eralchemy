a7d9ee8a6d87353b55cb21c30b07e225080303e9
Tony Hirst <tony.hirst@gmail.com>
2018-02-13 18:02:56 +0000

    Add support for including / excluding tables

--- eralchemy_magic/eralchemy.py
+++ eralchemy_magic/eralchemy.py
@@ -5,6 +5,7 @@ from IPython.core.display import Image, HTML, SVG
 from argparse import ArgumentParser
 import shlex
 
+from csv import reader
 from eralchemy import render_er
 
 @magics_class
@@ -21,13 +22,19 @@ class EralchemyMagic(Magics):
         parser.add_argument('-o', '--output_type', default='svg')
         parser.add_argument('-f', '--filestub', default='erd_from_db')
         parser.add_argument('-c', '--connection_string', default=None)
+        parser.add_argument('-i', '--include', default=None)
+        parser.add_argument('-x', '--exclude', default=None)
         
         args = parser.parse_args(shlex.split(line))
         
         if args.connection_string is None: return
         fname='{}.{}'.format(args.filestub,args.output_type)
-
-        return render_er(args.connection_string, fname )
+        
+        include = [i for i in reader([args.include])][0] if args.include is not None else None
+        exclude = [i for i in reader([args.exclude])][0] if args.exclude is not None else None
+        retval  = render_er(args.connection_string, fname, 
+                            include = include, exclude=exclude) 
+        return retval
 
 
 def load_ipython_extension(ipython):
